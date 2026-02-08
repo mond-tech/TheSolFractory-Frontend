8e69ccec2b441fd2cf7603a178bfa1afac0f93e9
MondTech <support@mondtech.in>
2026-02-08 17:14:34 +0530

    Glassy look of cone

--- src/components/homepage/shared/CarouselCanvas.tsx
+++ src/components/homepage/shared/CarouselCanvas.tsx
@@ -1,31 +1,31 @@
-"use client";
-import * as THREE from "three";
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { MotionValue } from "framer-motion";
-import { CarouselScene } from "./CarouselScene";
-import { PostFX } from "./Atmosphere";
+// "use client";
+// import * as THREE from "three";
+// import React, { Suspense } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { MotionValue } from "framer-motion";
+// import { CarouselScene } from "../shared/CarouselScene";
+// import { PostFX } from "./Atmosphere";
 
-export default function CarouselCanvas({
-  scrollProgress,
-}: {
-  scrollProgress: MotionValue<number>;
-}) {
-  return (
-    <Canvas
-      camera={{ position: [0, 2, 18], fov: 35 }}
-      gl={{
-        physicallyCorrectLights: true,
-        toneMapping: THREE.ACESFilmicToneMapping,
-        toneMappingExposure: 1.1,
-        outputColorSpace: THREE.SRGBColorSpace,
-      }}
-      shadows
-    >
-      <Suspense fallback={null}>
-        <CarouselScene scrollProgress={scrollProgress} />
-        <PostFX />
-      </Suspense>
-    </Canvas>
-  );
-}
+// export default function CarouselCanvas({
+//   scrollProgress,
+// }: {
+//   scrollProgress: MotionValue<number>;
+// }) {
+//   return (
+//     <Canvas
+//       camera={{ position: [0, 2, 18], fov: 35 }}
+//       gl={{
+//         physicallyCorrectLights: true,
+//         toneMapping: THREE.ACESFilmicToneMapping,
+//         toneMappingExposure: 1.1,
+//         outputColorSpace: THREE.SRGBColorSpace,
+//       }}
+//       shadows
+//     >
+//       <Suspense fallback={null}>
+//         <CarouselScene scrollProgress={scrollProgress} />
+//         <PostFX />
+//       </Suspense>
+//     </Canvas>
+//   );
+// }
